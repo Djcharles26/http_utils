c2a16d81012e0221fa69c5f05d24a8cdd4a3d76a
Djcharles26 <jlaraanaya@gmail.com>
2022-05-17 18:10:20 -0500

    Updated tests with new methods

--- test/body_utils_test.dart
+++ test/body_utils_test.dart
@@ -1,19 +1,75 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http_request_utils/body_utils.dart';
 
+class TestClass {
+  String id;
+  int integer;
+  String name;
+  String lastname;
+
+  TestClass({
+    required this.id,
+    required this.integer,
+    required this.name,
+    required this.lastname
+  });
+
+  factory TestClass.fromJson (dynamic json) {
+    return TestClass (
+      id: jsonField<String> (json, ["_id", "\$oid"], nullable: false),
+      integer: jsonField<int> (json, ["integer"],  nullable: false),
+      name: jsonField<String> (json, ["name"],  nullable: false),
+      lastname: jsonField<String> (json, ["lastname",],  nullable: false),
+    );
+  }
+}
+
+class TestListClass {
+  int index;
+  String text;
+
+  TestListClass({
+    required this.index,
+    required this.text
+  });
+
+  factory TestListClass.fromJson (dynamic json) {
+    return TestListClass (
+      index: jsonField<int> (json, ["index"],  nullable: false),
+      text: jsonField<String> (json, ["text"], nullable: false)
+    );
+  }
+}
+
 void main() {
   final json = {
     "_id": {
       "\$oid": "123"
     },
+    "stringList": ["Hello", "World"],
     "organization": null,
     "integer": 15,
     "number": 0.45,
     "name": "Juan",
     "lastname": "Lara",
     "nullValue": null,
-    "nonNullValue": null
+    "nonNullValue": null,
+    "classList": [
+      {
+        "index": 0,
+        "text": "Hola"
+      },
+      {
+        "index": 1,
+        "text": "Hello"
+      },
+      {
+        "index": 2,
+        "text": "Hallo"
+      }
+    ]
   };
+
   test('Check for a json with all files as correct types', () {
     expect(jsonField<String> (json, ["name",],  nullable: false), "Juan");
     expect(jsonField<int> (json, ["integer",],  nullable: false), 15);
@@ -22,6 +78,25 @@ void main() {
     expect(jsonField<String> (json, ["nullValue",]), null);
     expect(jsonField<String> (json, ["organization", "\$oid"], defaultValue: "Is null"), "Is null");
     expect (() => jsonField<String> (json, ["jsonBody"],  nullable: false), throwsA(isA<BodyException>()));
+  });
+
+  test('Check for list json fields', () {
+    expect (jsonListField<String>(json, ["stringList"], nullable: false), <String>["Hello", "World"]);
+    expect (jsonListField<String>(json, ["string_list"]), null);
+    expect (jsonListField<TestListClass>(json, ["classList"], map: TestListClass.fromJson), isA<List<TestListClass>> ());
+  });
+
+  test('Check for list json fields with incorrect field types', () {
+    expect (() => jsonListField<int> (json, ["stringList"], nullable: false), throwsA (isA<BodyException> ()));
+  });
+
+  test ('Check for class json fields', () {
+    expect (jsonClassField<TestClass>(json, [], TestClass.fromJson, nullable: false), isA<TestClass> ());
+    expect (jsonClassField<TestClass>(json, ["object"], (item) => TestClass.fromJson (item)), null);
+  });
+
+  test ('Check for class json fields with incorrect field type', () {
+    expect (() => jsonClassField<TestListClass> (json, [], TestListClass.fromJson, nullable: false), throwsA(isA<BodyException> ()));
   });
 
   test ("Check for exceptions with incorrect file types", () {
